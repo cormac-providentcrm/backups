4debb8f9562107af12d61e86b43301cdb93bdb57
cormac-providentcrm <104010579+cormac-providentcrm@users.noreply.github.com>
2023-04-12 16:27:19 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Cormac O'Riordan 12/04/2023
+#Backup script to be run nightly, logs successes/failures
+
+dat="$(date +"%d-%m-%y")"
+ldat="$(date +"%H:%M:%S %d-%m-%y")"
+name="$(eval 'echo backup.$dat.sql.gz')"
+dest="/mnt/share/backup"
+file=${dest}/${name}
+
+mysqldump --set-gtid-purged=OFF --routines --triggers SugarCRM | gzip  > $file
+
+if [ "$?" -eq 0 ]
+then
+    echo "Dump successful "$ldat >> log.file
+else
+    echo "Dump failed, check mysql logs "$ldat >> log.file 
+fi
+
+if [ -f "$name" ]
+then
+    echo "Dump looks OK "$ldat >> log.file
+else
+    echo "Dump not found, check mysql logs "$ldat >> log.file
+fi
